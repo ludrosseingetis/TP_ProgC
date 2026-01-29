a88164e93d40c9ca910fe1b728f300250f2b81ff
flavien <flavien.petton@gmail.com>
2026-01-29 10:41:43 +0000

    Commit depuis la2b

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 int main(){
-int a,b,c,d;
+int a,b,c,d,e,f;
 a = 16;
 b = 3;
 c = a+b;
 printf("L'addition de %d et %d = %d \n",a,b,c);
 d = b-a;
 printf("La soustraction de %d et %d est %d\n", b, a, d);
+e= a*b;
+printf("La multiplication de %d et %d est %d \n",a,b,e);
+f= a%b;
+printf("Le reste de la division de %d par %d est : %d \n",a,b,f);
 }
 
