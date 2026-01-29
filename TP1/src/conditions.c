8c463733a53c9f5eae84c54a202b1915ba421b57
flavien <flavien.petton@gmail.com>
2026-01-29 11:15:31 +0000

    tt

---
+++ TP1/src/conditions.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    // Standard for loop: (start; condition; step)
+    for (i = 0; i < 1000; i++) {
+        printf("%d \n", i);
+    }
+    return 0;
+}
