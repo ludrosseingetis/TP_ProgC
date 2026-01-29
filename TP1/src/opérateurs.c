5c4f511bcaf117a560c4d82c57835611a698235d
flavien <flavien.petton@gmail.com>
2026-01-29 09:56:03 +0000

    Commit depuis lab

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,11 @@
-
+#include <stdio.h>
+int main(){
+int a,b,c,d;
+a = 16;
+b = 3;
+c = a+b;
+printf("L'addition de %d et %d = %d \n",a,b,c);
+d = b-a;
+printf("La soustraction de %d et %d est %d\n", b, a, d);
+}
 
